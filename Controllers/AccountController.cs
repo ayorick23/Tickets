f06a14a5c954ad34b8a05b569195c9ed4b166008
Dereck Méndez <mayorickhenry@gmail.com>
2026-03-30 18:32:25 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using Tickets.Application.Services;
+using Tickets.Domain.Entities;
+using Tickets.Domain.Interfaces;
+using Tickets.DTOs;
+
+namespace Tickets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        readonly AuthService _authservice;
+
+        public AccountController(AuthService authService)
+        {
+            _authservice = authService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUserDto registerUserDto)
+        {
+            Usuario newUser = new Usuario()
+            {
+                Email = registerUserDto.Email,
+                FirstName = registerUserDto.FirstName,
+                LastName = registerUserDto.LastName,
+                Tel = registerUserDto.Phone,
+                Password = registerUserDto.Password
+            };
+
+            var result = await _authservice.RegisterUser(newUser);
+            return Ok("Registro Exitoso");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var result = await _authservice.Login(loginDto.Email, loginDto.Password, loginDto.RememberMe);
+            
+            return Ok(new { Token = result });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Tickets.Application.Services;
+using Tickets.Domain.Interfaces;
+using Tickets.Infraestructure.Identity;
+using Tickets.Infraestructure.Persistence;
+using Tickets.Infraestructure.Persistence.Repositories;
+using Tickets.Infrastructure.Security;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<AppIdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Inyectando servicios
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<IJwtService, JwtService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtIssuer"],
+            ValidAudience = builder.Configuration["JwtAudience"],
+            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtKey"]))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
